88eef36c8b993c093a40f0a10eb01ec45b83fffc
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-12 17:05:24 -0600

    API(1)

---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,10 @@
+﻿using Chef_KhAI.Shared;
+
+namespace Chef_KhAI.Server.Services
+{
+	public class IOpenAIAPI
+	{
+		Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+
+	}
+}
