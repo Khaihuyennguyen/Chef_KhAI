74a50c82cbb28344babf4950e81e28296e3f8a3c
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-11 23:25:04 -0600

    create controler

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,18 @@
+﻿using AiChef.Server.Data;
+using Chef_KhAI.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chef_KhAI.Server.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class RecipeController : ControllerBase
+	{
+		[HttpPost, Route("GetRecipeIdeas")]
+		public async Task<ActionResult<List<Idea>>> GetRecipeIdeas()
+		{
+			return SampleData.RecipeIdeas;
+		}
+	}
+}
